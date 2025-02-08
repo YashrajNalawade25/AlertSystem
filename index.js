50be6ca94503b32f3ca665592ffc911b5936128b
yashrajnalawade9 <sopan.yashraj22@vit.edu>
2025-02-08 23:59:09 +0530

    u8

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 // app.use(express.static(path.join(__dirname, "views")));
 
 // Supabase setup
